7ff7df3bab8093a5cff9246ec6a6eec06ebbd634
Sergey30000 <Sergey2018sar@yandex.ru>
2024-03-30 23:10:46 +0400

    update test

--- index.js
+++ index.js
@@ -23,55 +23,12 @@ app.use(express.static(path.resolve(__dirname, "static")));
 app.use('/api',router);
 app.use(errorHandler);
 
-app.get('/test', function(request,response){
-    response.send('Hello Test');
+app.get('/api/hello', (req,res) => {
+    res.status(200).send('Hello Test');
 });
 
-app.listen(PORT, () => console.log(`Server was started. Port: ${PORT}`));
-/*
-const start = async () => {
-    let connected = false;
-    let retryCount = 0;
-    const maxRetries = 5;
-    
-    const connectToDatabase = async () => {
-        try {
-            await sequelize.authenticate();
-            connected = true;
-        } catch (error) {
-            console.log(error);
-            if (retryCount < maxRetries){
-                retryCount++;
-                setTimeout(connectToDatabase, 2000);
-            }
-            
-        }
-    };
+const server = app.listen(PORT, () => console.log(`Server was started. Port: ${PORT}`));
 
-    await connectToDatabase();
-
-    if (!connected) {
-        process.exit(1);
-    }
-    
-    try{
-
-        await sequelize.sync();
-        const server = app.listen(PORT, () => console.log(`Server was started. Port: ${PORT}`));
-        function closeApp(){
-            db.connection.close().then(x=>{
-	        server.close(()=>
-	            console.log('Server stop')
-	            );
-	        }
-	    );
-        }
-        procces.on('SIGTERM',closeApp);
-        procces.on('SIGINT',closeApp);
-        
-    } catch (e){
-        console.log(e);
-    }
-}
-
-start();*/
+module.exports = server;
+//npx mocha
+//curl -X GET http://localhost:3001/api/hello
